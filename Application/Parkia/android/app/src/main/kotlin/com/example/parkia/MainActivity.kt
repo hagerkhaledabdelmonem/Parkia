4dcee297f358c96f4801759f3f7a832ba1191749
hagerkhaledabdelmonem <hagerkhaledabdelmonem@gmail.com>
2024-08-15 12:02:21 +0300

    upload

--- Application/Parkia/android/app/src/main/kotlin/com/example/parkia/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.parkia
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Application/Parkia/lib/constants.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:ui';
-
-const darkestOne = Color(0xff171965);
-const darkBlue = Color(0xff2e3192);
-const midBlue = Color(0xff396cb6);
-const lightBlue = Color(0xff74a9f0);
-const TextBoxColor = Color(0xff90a2c5);
-const shadowColor = Color(0xff4d77a6);
-const whiteColor = Color(0xfff6faff);
